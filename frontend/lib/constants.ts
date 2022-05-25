b8005ba7fcf1df13c6a0372667cd0959b7acf6a4
Callum McIntyre <callum@pointer.gg>
2022-05-25 14:18:52 +0100

    Add provide liquidity UI

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -6,3 +6,7 @@ export const appDetails: AuthOptions["appDetails"] = {
 }
 
 export const contractOwnerAddress = "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM"
+
+export const exchangeContractName = "beanstalk-exchange"
+
+export const microstacksPerSTX = 1_000_000
